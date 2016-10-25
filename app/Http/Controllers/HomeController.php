9dbb0948d3e6bbdd46ffeb8a20c3597098504f58
Irakli Tchitadze <test@pexuniverse.com>
2016-10-25 16:53:37 -0400

    How many PERCENT is passed after starting new working week(after salary)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Buglinjo\HelloComposer\Hello;
 
 class HomeController extends Controller
 {
     public function index(){
-        return Hello::world();
+        date_default_timezone_set('America/New_York');
+
+        $startDate = Carbon::create(2016,10,6,18,0,0);
+
+        $secondsInWorkingDay = 32400;
+
+        $bothWeekInSeconds = 259200;
+
+        $secondsLeftUntilEndOfDay = 0;
+
+        $workingWeekEnd = Carbon::now()->endOfWeek()->subDays(3)->addSecond();
+
+        $startOfWorkingDay = Carbon::now()->startOfDay()->addHours(9);
+        $endOfWorkingDay = Carbon::now()->endOfDay()->subHours(6)->addSecond();
+        $now = Carbon::now();
+
+        $weekNumber = $now->diffInWeeks($startDate)%2 == 0 ? 1 : 2;
+
+        $daysLeftUntilSalary = $now->copy()->endOfDay()->diffInDays($workingWeekEnd);
+
+        if($weekNumber == 1){
+            $daysLeftUntilSalary = $now->copy()->endOfDay()->diffInDays($workingWeekEnd) + 4;
+        }
+
+        if($now > $startOfWorkingDay && $now < $endOfWorkingDay){
+            $secondsLeftUntilEndOfDay = $now->diffInSeconds($endOfWorkingDay);
+        }elseif($now < $startOfWorkingDay){
+            $daysLeftUntilSalary++;
+        }
+
+        $secondsLeftUntilSalary = ($daysLeftUntilSalary * $secondsInWorkingDay) + $secondsLeftUntilEndOfDay;
+        $secondsPassedAfterStartingWeek = $bothWeekInSeconds - $secondsLeftUntilSalary;
+
+        $percent = (double)($secondsPassedAfterStartingWeek * 100 / $bothWeekInSeconds);
+        $data['percent'] = number_format($percent, 2, ".", "")."%";
+
+        return view('welcome', $data);
     }
 }
